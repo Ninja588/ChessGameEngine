0d8509d7f623d5a1ab2da0611270f27668da5cdb
Kamil <ninja59234@gmail.com>
2024-12-30 17:18:15 +0100

    zbedne pliki usuniete

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        //ChessBoard chessBoard = new ChessBoard();
-        //ChessGameGUI chessGameGUI = new ChessGameGUI(false);
-        new ChessMenu();
-    }
-}
